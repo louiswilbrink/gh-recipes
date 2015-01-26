ee2148728dbb0548a92d366d69cc7e54f08b0b0d
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-01-26 10:25:01 -0500

    install nginx during gh recipes

--- nginx/recipes/gh-api.rb
+++ nginx/recipes/gh-api.rb
@@ -1,3 +1,5 @@
+package "nginx"
+
 template "gh.conf" do
   path "#{node[:nginx][:dir]}/conf.d/gh.conf"
   source "gh.conf.erb"
--- nginx/recipes/gh-web.rb
+++ nginx/recipes/gh-web.rb
@@ -1,3 +1,5 @@
+package "nginx"
+
 template "gh.web.conf" do
   path "#{node[:nginx][:dir]}/conf.d/gh.web.conf"
   source "gh.web.conf.erb"
