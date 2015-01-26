36c6b147ba7674ac201496d7b0cfe7caa3fcb5c4
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-01-26 10:57:36 -0500

    replaced deleted gh.api.conf.erb

--- nginx/recipes/gh-api.rb
+++ nginx/recipes/gh-api.rb
@@ -1,8 +1,8 @@
 package "nginx"
 
-template "gh.conf" do
-  path "#{node[:nginx][:dir]}/conf.d/gh.conf"
-  source "gh.conf.erb"
+template "gh.api.conf" do
+  path "#{node[:nginx][:dir]}/conf.d/gh.api.conf"
+  source "gh.api.conf.erb"
   owner "root"
   group "root"
   mode 0644
